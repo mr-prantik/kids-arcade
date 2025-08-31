d6c89a227499e1d1f7b46493207e644e6d4c69a9
mr-prantik <prantikmahata9211@gmail.com>
2025-08-31 12:46:06 +0530

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,3 @@
-// import GameGrid from "@/components/GameGrid";
-
-// export default function Home() {
-//   return (
-//     <main className="min-h-screen bg-gradient-to-b from-pink-100 to-blue-100">
-//       <h1 className="p-6 text-center text-4xl font-extrabold text-purple-700">
-//         Kids Mini-Games 🎮
-//       </h1>
-//       <GameGrid />
-//     </main>
-//   );
-// }
-
-
 "use client";
 
 import Link from "next/link";
